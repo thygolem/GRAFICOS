7d201e83e44659746994bfb4922980352d7c1b39
thygolem <thygolem@gmail.com>
2021-07-14 11:03:49 +0200

    3rd

--- G_simple.py
+++ G_simple.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 '''
-Iván Rodríguez - 2017
-Código generado para el canal de YouTube Piensa 3D
+
+Basado en el código de Iván Rodríguez
+https://www.youtube.com/watch?v=MvWCIbpvykE&list=PLG8UtYUFOQj4PwdP1SusjENu-fPOh7bnZ&index=4
+
 '''
 
 # Importamos los módulos necesarios
@@ -12,17 +14,40 @@ from matplotlib import pyplot as plt
 
 # Generamos los datos para el gráfico
 plt.ion()
+
+
+
 x = np.array(range(500))*0.1
 y = np.zeros(len(x))
 for i in range(len(x)):
     y[i] = math.sin(x[i])
 
 
+x_2 = np.array(range(500))*0.1
+y_2 = np.zeros(len(x_2))
+for j in range(len(x_2)):
+    y_2[j] = math.cos(x_2[j])
+
 # Creamos el gráfico
-plt.plot(x,y)
+plt.plot(x,y,'y--',x_2,y_2,'c_')
+
+'''
+Colores Matplotlib: 'r', 'g', 'b', 'c', 'm' 'y', 'k', 'w'
+'''
+
+'''
+Tipos de lineas: '-', '--', '-_', ':', '_', 'O', 'v', '^', '<','>', '1', '2', '3', '4', 's', 'p', '*', 'h', 'H', '+', 'x', 'D', 'd', 'l'
+'''
+
+
 plt.xlabel("Coordenada X")
 plt.ylabel("Coordenada Y")
+
+plt.legend(['Seno', 'Coseno'])
+
 plt.title("SENO ex: 1")
+
+
 
 plt.ioff()
 plt.show()
